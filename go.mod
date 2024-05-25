e6407c8dff72944c160995721affa4f5e3371681
David Juhasz <djjuhasz@gmail.com>
2024-05-24 17:31:53 -0700

    Add config validation

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.3
 
 require (
 	github.com/go-logr/logr v1.4.1
+	github.com/go-playground/validator/v10 v10.20.0
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.18.2
 	github.com/stretchr/testify v1.9.0
@@ -16,7 +17,10 @@ require (
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/facebookgo/clock v0.0.0-20150410010913-600d898af40a // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/go-logr/zapr v1.2.4 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/mock v1.6.0 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
@@ -24,6 +28,7 @@ require (
 	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.19.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pborman/uuid v1.2.1 // indirect
@@ -40,6 +45,7 @@ require (
 	go.temporal.io/api v1.32.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
 	golang.org/x/exp v0.0.0-20231127185646-65229373498e // indirect
 	golang.org/x/net v0.24.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,9 +1,12 @@
 package config
 
 import (
+	"errors"
 	"fmt"
+	"os"
 	"strings"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/spf13/viper"
 )
 
@@ -14,23 +17,50 @@ type ConfigurationValidator interface {
 type Configuration struct {
 	Debug      bool
 	Verbosity  int
-	SharedPath string
+	SharedPath string `validate:"required"`
 	Temporal   Temporal
 	Worker     WorkerConfig
 }
 
 type Temporal struct {
-	Address      string
-	Namespace    string
-	TaskQueue    string
-	WorkflowName string
+	// Address is the Temporal server host and port (default: "localhost:7233").
+	Address string
+
+	// Namespace is the Temporal namespace the preprocessing worker should run
+	// in (default: "default").
+	Namespace string
+
+	// TaskQueue is the Temporal task queue from which the preprocessing worker
+	// will pull tasks.
+	TaskQueue string `validate:"required"`
+
+	// WorkflowName is the name of the preprocessing Temporal workflow.
+	WorkflowName string `validate:"required"`
 }
 
 type WorkerConfig struct {
-	MaxConcurrentSessions int
+	// MaxConcurrentSessions limits the number of workflow sessions the
+	// preprocessing worker can handle simultaneously (default: 1).
+	MaxConcurrentSessions int `validate:"gte=1"`
 }
 
-func (c Configuration) Validate() error { return nil }
+func (c Configuration) Validate() error {
+	var errs error
+
+	validate := validator.New(validator.WithRequiredStructEnabled())
+	if err := validate.Struct(c); err != nil {
+		for _, e := range err.(validator.ValidationErrors) {
+			switch e.Tag() {
+			case "required":
+				errs = errors.Join(errs, fmt.Errorf("%s: missing required value", e.Field()))
+			default:
+				errs = errors.Join(errs, e)
+			}
+		}
+	}
+
+	return errs
+}
 
 func Read(config *Configuration, configFile string) (found bool, configFileUsed string, err error) {
 	v := viper.New()
@@ -47,28 +77,36 @@ func Read(config *Configuration, configFile string) (found bool, configFileUsed 
 	v.SetDefault("Worker.MaxConcurrentSessions", 1)
 
 	if configFile != "" {
+		// Viper will not return a viper.ConfigFileNotFoundError error when
+		// SetConfigFile() is passed a path to a file that doesn't exist, so we
+		// need to check ourselves.
+		if _, err := os.Stat(configFile); errors.Is(err, os.ErrNotExist) {
+			return false, "", fmt.Errorf("configuration file not found: %s", configFile)
+		}
+
 		v.SetConfigFile(configFile)
 	}
 
-	err = v.ReadInConfig()
-	_, ok := err.(viper.ConfigFileNotFoundError)
-	if !ok {
-		found = true
-	}
-	if found && err != nil {
-		return found, configFileUsed, fmt.Errorf("failed to read configuration file: %w", err)
+	if err = v.ReadInConfig(); err != nil {
+		switch err.(type) {
+		case viper.ConfigFileNotFoundError:
+			return false, "", err
+		default:
+			return true, "", fmt.Errorf("failed to read configuration file: %w", err)
+		}
 	}
 
 	err = v.Unmarshal(config)
 	if err != nil {
-		return found, configFileUsed, fmt.Errorf("failed to unmarshal configuration: %w", err)
+		return true, "", fmt.Errorf("failed to unmarshal configuration: %w", err)
 	}
 
 	if err := config.Validate(); err != nil {
-		return found, configFileUsed, fmt.Errorf("failed to validate the provided config: %w", err)
+		return true, "", errors.Join(
+			fmt.Errorf("invalid configuration:"),
+			err,
+		)
 	}
 
-	configFileUsed = v.ConfigFileUsed()
-
-	return found, configFileUsed, nil
+	return true, v.ConfigFileUsed(), nil
 }
